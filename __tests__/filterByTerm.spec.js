3d610f30416a82932a89c43facf563aa15938ec2
Robin Lee <robinleeaus@gmail.com>
2020-05-09 00:15:07 +1000

    1. test for the search term "uRl" 2. test for an empty search term 3. test for empty input array 4. add code coverage

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,16 +1,7 @@
-
-function filterByTerm(inputArr, searchTerm) {
-    const regex = new RegExp(searchTerm, "i");
-    return inputArr.filter(function(arrayElement) {
-        // return arrayElement.url.match(searchTerm);
-        return arrayElement.url.match(regex);
-    });
-}
+const filterByTerm = require("../src/filterByTerm");
 
 describe("Filter function", () => {
-    // test stuff
     test("it should filter by a search term (link)", () => {
-        // actual test
         // define a simple input, an array of objects
         const input = [
             { id: 1, url: "https://www.url1.dev" },
@@ -19,23 +10,46 @@ describe("Filter function", () => {
         ];
         // define the expected result
         const output = [{ id: 3, url: "https://www.link3.dev" }];
-        const output2 = [
+
+        expect(filterByTerm(input, "link")).toEqual(output);
+        expect(filterByTerm(input, "LINK")).toEqual(output); // Test "LINK"
+    });
+
+    test("it should filter by a search term (uRL)", () => {
+        // define a simple input, an array of objects
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }
+        ];
+        // define the expected result
+        const output = [
             { id: 1, url: "https://www.url1.dev" },
             { id: 2, url: "https://www.url2.dev" }
         ];
 
-        const output3 = [
+        expect(filterByTerm(input, "uRL")).toEqual(output); // Test "uRL"
+    });
+
+    test("it should throw when searchTerm is empty string", () => {
+        // define a simple input, an array of objects
+        const input = [
             { id: 1, url: "https://www.url1.dev" },
             { id: 2, url: "https://www.url2.dev" },
             { id: 3, url: "https://www.link3.dev" }
         ];
 
-        expect(filterByTerm(input, "link")).toEqual(output);
+        expect(() => {
+            filterByTerm(input, "")
+        }).toThrowError(Error("searchTerm cannot be empty")); // Test empty string
+    });
 
-        expect(filterByTerm(input, "LINK")).toEqual(output); // Test "LINK"
+    test("it should throw when inputArr is empty array", () => {
+        // define a empty input array
+        const input = [];
 
-        expect(filterByTerm(input, "uRL")).toEqual(output2); // Test "uRL"
-        // console.log(filterByTerm(input, ));
-        expect(filterByTerm(input, "")).toEqual(output3); // Test empty string
+        expect(() => {
+            filterByTerm(input, "link")
+        }).toThrowError(Error("inputArr cannot be empty")); // Test empty input array
     });
 });
