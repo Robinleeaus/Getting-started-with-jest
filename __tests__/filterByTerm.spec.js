ff19ac69d56f081258e7105d66f30e35bfd6d0d6
Robin Lee <robinleeaus@gmail.com>
2020-05-09 19:36:15 +1000

    make one test fail

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -42,14 +42,18 @@ describe("Filter function", () => {
         // expect(() => {
         //     filterByTerm(input, "")
         // }).toThrowError(Error("searchTerm cannot be empty")); // Test empty string
+
+        expect(
+            filterByTerm(input, "")
+        ).toThrowError(Error("searchTerm cannot be empty")); // Test empty string
     });
 
-    // test("it should throw when inputArr is empty array", () => {
-    //     // define a empty input array
-    //     const input = [];
+    test("it should throw when inputArr is empty array", () => {
+        // define a empty input array
+        const input = [];
 
-    //     expect(() => {
-    //         filterByTerm(input, "link")
-    //     }).toThrowError(Error("inputArr cannot be empty")); // Test empty input array
-    // });
+        expect(() => {
+            filterByTerm(input, "link")
+        }).toThrowError(Error("inputArr cannot be empty")); // Test empty input array
+    });
 });
